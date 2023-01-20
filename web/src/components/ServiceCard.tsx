2e6460edd9beb498fd1198e61b28ad52c91ec668
EnergyCube <vincent.mardirossian@epitech.eu>
2023-01-20 03:20:47 +0100

    feat: wip Button and ServiceCard

---
+++ web/src/components/ServiceCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from './Button';
+
+export interface IServiceCardProps {
+  img: string;
+  title: string;
+  description: string;
+  buttonLabel: string;
+  onClick: () => void;
+}
+
+function ServiceCard({ img, title, description, buttonLabel, onClick }: IServiceCardProps) {
+  return (
+    <div>
+      <Card>
+        <CardContent>
+          <div>
+            <div>
+              <img src={img} alt={title} style={{ maxHeight: '100px', maxWidth: '150px' }} />
+            </div>
+          </div>
+          <Typography variant="h4" gutterBottom>
+            {title}
+          </Typography>
+          <Typography color="text">{description}</Typography>
+        </CardContent>
+        <CardActions style={{ justifyContent: 'center' }}>
+          <Button color="primary" text="Click me" onClick={() => alert('Hello')} />
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
+export default ServiceCard;
