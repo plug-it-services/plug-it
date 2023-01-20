3ab77934cd706eb24af1b6b05d8a7ab75437ce59
EnergyCube <vincent.mardirossian@epitech.eu>
2023-01-20 03:20:14 +0100

    feat: project architecture

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,17 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './components/Button';
+import AccountTile from './components/AccountTile';
+import ServiceCard from './components/ServiceCard';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header title="Plug-it" area="AREAs" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <br></br>
+        <Button color="primary" text="Click me" onClick={() => alert('Hello')} />
+        <br></br>
+        <Button color="secondary" text="Click me" onClick={() => alert('Hello')} />
+        <br></br>
+        <AccountTile name="Jean Michel" email="jeanmichel@plugit.org" />
+        <br></br>
+        <ServiceCard
+          img={'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png'}
+          title={'Youtube'}
+          description={'Youtube is a video sharing platform.'}
+          buttonLabel={'Connect'}
+          onClick={() => alert('Add')}
+        />
+        <br></br>
       </header>
     </div>
   );
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './components/Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
