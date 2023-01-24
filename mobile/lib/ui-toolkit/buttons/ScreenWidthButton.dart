5990c94eb51509a12587fe2e046a331769a3451b
Tristan <tristan.gremont@epitech.eu>
2023-01-25 00:15:29 +0100

    feat(mobile/view): added button widget to tool-kit

---
+++ mobile/lib/ui-toolkit/buttons/ScreenWidthButton.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/ui-toolkit/PlugItStyle.dart';
+
+
+class ScreenWidthButton extends StatefulWidget {
+  final String label;
+  final double size;
+  final Color? color;
+  final double? height;
+  final Color? pressedColor;
+  final void Function()? callback;
+
+  const ScreenWidthButton({super.key, this.label = "", this.size = 15, this.callback, this.color, this.pressedColor, this.height});
+
+  @override
+  State createState() => _StateScreenWidthButton();
+}
+class _StateScreenWidthButton extends State<ScreenWidthButton>{
+  bool pressed = false;
+
+  void onTap() {
+    setState(() {
+      pressed = true;
+    });
+  }
+
+  void onEnd() {
+    setState(() {
+      pressed = false;
+      widget.callback!();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: GestureDetector(
+              onTap: onTap,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                onEnd: onEnd,
+                height: widget.height ?? 55,
+                decoration: BoxDecoration(
+                  color: (!pressed) ? (widget.color ?? PlugItStyle.buttonColor) : (widget.pressedColor ?? PlugItStyle.buttonColorPressed),
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                    child: Text(
+                        widget.label,
+                        style: TextStyle(color: Colors.white, fontSize: widget.size, fontWeight: FontWeight.bold)
+                      ),
+                    )
+                ),
+              ),
+    );
+  }
+}
