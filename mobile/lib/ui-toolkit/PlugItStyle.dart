ac89dec685e49d53eb85b535aad9fbbed57485ad
Tristan <tristan.gremont@epitech.eu>
2023-01-25 00:17:08 +0100

    feat(mobile/view): added app style class to tool-kit

---
+++ mobile/lib/ui-toolkit/PlugItStyle.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PlugItStyle {
+  static var foregroundColor = Colors.grey[100];
+  static const backgroundColor = Colors.white70;
+
+  static var primaryColor = Colors.blue.shade700;
+  static var secondaryColor = Colors.grey[300];
+
+  static var buttonColor = Colors.blue.shade700;
+  static var buttonColorPressed = Colors.blue.shade300;
+
+  static var validationColor = Colors.green[600];
+
+  static var cardColor = Colors.blue.shade500;
+
+
+  static const inputHintStyle = TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.normal, color: Colors.black);
+  static const inputTextStyle = TextStyle(fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black);
+  static const titleStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 52);
+  static const subtitleStyle = TextStyle(fontWeight: FontWeight.w500, fontSize: 20);
+
+}
