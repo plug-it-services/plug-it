11c7dc0081fe0e425b3a2622f7e7dfb3fa5027b7
Tristan <tristan.gremont@epitech.eu>
2023-01-26 21:40:11 +0100

    feat(mobile/view): added custom IconSwitch to tool-kit

---
+++ mobile/lib/ui-toolkit/buttons/IconButtonSwitch.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:mobile/models/Plug.dart';
+import 'package:mobile/models/Service.dart';
+import 'package:mobile/ui-toolkit/PlugItStyle.dart';
+import 'package:mobile/ui-toolkit/buttons/ScreenWidthButton.dart';
+import 'package:mobile/models/Trigger.dart';
+
+
+class IconButtonSwitch extends StatefulWidget {
+  final void Function(bool value)? onChange;
+  final Icon falseIcon;
+  final Icon trueIcon;
+  final bool state;
+
+  const IconButtonSwitch({super.key,
+    required this.falseIcon,
+    required this.trueIcon,
+    this.onChange,
+    this.state = false
+  });
+
+  @override
+  State createState() => _StateIconButtonSwitch();
+}
+class _StateIconButtonSwitch extends State<IconButtonSwitch>{
+  bool state = false;
+  bool changed = false;
+
+  void onTap() {
+    setState(() {
+      changed = true;
+      state = !state;
+      widget.onChange!(state);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!changed) {
+      state = widget.state;
+    }
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        child: state ? widget.trueIcon : widget.falseIcon
+      )
+    );
+
+  }
+}
