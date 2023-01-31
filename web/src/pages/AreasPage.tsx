53fb1d2b078083e4f779d71582b929646844eb66
EnergyCube <vincent.mardirossian@epitech.eu>
2023-01-31 10:02:44 +0100

    feat: wip AREAs list integration in AREAs pages

--- web/src/pages/AreasPage.tsx
+++ web/src/pages/AreasPage.tsx
@@ -1,63 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Typography } from '@mui/material';
+import {Grid, Typography} from '@mui/material';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import SearchBar from '../components/SearchBar';
 import AreaCard from '../components/AreaCard';
+import { getPlugs, Plug } from '../utils/api';
+import ServiceCard from "../components/ServiceCard";
 
-const AreasPage = () => (
-  // Title and SearchBar
-  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    <Header title="Plug-It" area="Areas" />
-    <br />
-    <Typography variant="h4" fontWeight="bold" color={'primary'}>
-      Areas
-    </Typography>
-    <br />
-    <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-      <SearchBar
-        onChange={() => {}}
-        onSearch={() => {}}
-        defaultDummyValue="Search an area"
-        textColor="black"
-        backgroundColor="#EAF1FF"
-        borderColor="#EAF1FF"
-      />
-      <Button
-        color="primary"
-        text={'Add Area'}
-        onClick={() => {
-          window.location.href = '/areas/create';
-        }}
-      />
+const AreasPage = () => {
+  const [areas, setPlugs] = useState<Plug[]>([]);
+
+  useEffect(() => {
+    async function fetchPlugs() {
+      setPlugs(await getPlugs());
+    }
+    fetchPlugs();
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Header title="Plug-It" area="Areas" />
+      <br />
+      <Typography variant="h4" fontWeight="bold" color={'primary'}>
+        Areas
+      </Typography>
+      <br />
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+        <SearchBar
+          onChange={() => {}}
+          onSearch={() => {}}
+          defaultDummyValue="Search an area"
+          textColor="black"
+          backgroundColor="#EAF1FF"
+          borderColor="#EAF1FF"
+        />
+        <Button
+          color="primary"
+          text={'Add Area'}
+          onClick={() => {
+            window.location.href = '/areas/create';
+          }}
+        />
+      </div>
+      <br />
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+        <Grid container spacing={2} columns={3}>
+          {areas.map((area) => (
+            <Grid item key={area.name}>
+              <AreaCard
+                title={area.name}
+                buttonLabel={'Connect'}
+                onClick={() => {}}
+                date={'2001-09-11'}
+                iconList={area.icons}
+                key={area.name}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
-    <br />
-    <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-      <AreaCard
-        title={'Youtube'}
-        date={'2021-10-10'}
-        iconList={[
-          'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png',
-          'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png',
-          'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png',
-        ]}
-        buttonLabel={'Disable'}
-        onClick={() => {}}
-      />
-      <AreaCard
-        title={'Youtube'}
-        date={'2021-10-10'}
-        iconList={[
-          'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png',
-          'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png',
-          'https://clipart.info/images/ccovers/1590430652red-youtube-logo-png-xl.png',
-        ]}
-        buttonLabel={'Disable'}
-        onClick={() => {}}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default AreasPage;
