d6207ddbae92c02fafc5f14c7047d12021104cb9
Tristan <tristan.gremont@epitech.eu>
2023-02-04 16:42:42 +0100

    feat(mobile/view): added service card color from service model

--- mobile/lib/models/service/Service.dart
+++ mobile/lib/models/service/Service.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:mobile/models/types.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:mobile/ui-toolkit/PlugItStyle.dart';
 
 part 'Service.g.dart';
 
@@ -7,8 +9,9 @@ part 'Service.g.dart';
 class Service {
   String name, authType, icon;
   bool connected;
+  Color color;
 
-  Service({required this.name, required this.authType, required this.icon, required this.connected});
+  Service({required this.name, required this.authType, required this.icon, required this.connected, required this.color});
 
   factory Service.fromJson(Json json) => _$ServiceFromJson(json);
 
