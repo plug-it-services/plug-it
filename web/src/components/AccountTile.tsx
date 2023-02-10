b405315c03859e6f2f0af27c6ab1ec4c28df1bd0
EnergyCube <vincent.mardirossian@epitech.eu>
2023-02-10 23:18:24 +0100

    feat(web/userInfos): implemented User Infos from API to Header card

--- web/src/components/AccountTile.tsx
+++ web/src/components/AccountTile.tsx
@@ -3,16 +3,20 @@ import { Avatar, Card } from '@mui/material';
 
 export interface IAccountTileProps {
   name: string;
+  firstName: string;
+  id: number;
   email: string;
   onDisconnect: () => void;
 }
 
-function AccountTile({ name, email, onDisconnect }: IAccountTileProps) {
+function AccountTile({ name, firstName, id, email, onDisconnect }: IAccountTileProps) {
   return (
     <Card className={'card-generic-small'} onClick={onDisconnect}>
-      <Avatar sx={{ bgcolor: 'primary.main' }}>{name[0]}</Avatar>
+      <Avatar sx={{ bgcolor: 'primary.main' }}>{name.length > 0 ? name[0] : '?'}</Avatar>
       <div className={'account-card-separator'}>
-        <div>{name}</div>
+        <div>
+          {firstName} {name} ({id})
+        </div>
         <div>{email}</div>
       </div>
     </Card>
