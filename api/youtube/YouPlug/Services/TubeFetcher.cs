2a56cf897dbdd9fbeab32a424cea6247783fc5a0
EnergyCube <vincent.mardirossian@epitech.eu>
2023-02-19 11:20:40 +0100

    feat(yt/regist): WIP Youtube channels/videos query

---
+++ api/youtube/YouPlug/Services/TubeFetcher.cs
@@ -0,0 +1,78 @@
+﻿using YouPlug.Models;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Google.Apis.Services;
+using Microsoft.EntityFrameworkCore;
+using YouPlug.Dto.Youtube;
+
+namespace YouPlug.Services
+{
+    public class TubeFetcher
+    {
+        private YouTubeService youtubeService;
+
+        public TubeFetcher(string apiKey)
+        {
+            youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = apiKey,
+                ApplicationName = GetType().ToString()
+            });
+        }
+
+        public List<ChannelDto> GetSubscriptions(string channelId)
+        {
+            List<ChannelDto> channels = new();
+            var request = youtubeService.Subscriptions.List("snippet");
+            request.ChannelId = channelId;
+            request.Mine = true;
+
+            var response = request.Execute();
+            var subscriptions = response.Items;
+
+            foreach (var subscription in subscriptions)
+            {
+                channels.Add(new ChannelDto
+                {
+                    Id = subscription.Snippet.ResourceId.ChannelId,
+                    Title = subscription.Snippet.Title,
+                    Description = subscription.Snippet.Description,
+                    Thumbnail = subscription.Snippet.Thumbnails.High.Url
+                });
+            }
+
+            return channels;
+        }
+
+        public List<VideoDto> GetVideos(string channelId)
+        {
+            List<VideoDto> videos = new();
+            var request = youtubeService.Search.List("snippet");
+            request.ChannelId = channelId;
+            request.Order = SearchResource.ListRequest.OrderEnum.Date;
+            request.MaxResults = 5; // No need to query a lot of videos
+
+            var response = request.Execute();
+            var searchResults = response.Items;
+
+            foreach (var searchResult in searchResults)
+            {
+                if (searchResult.Id.Kind == "youtube#video")
+                {
+                    videos.Add(new VideoDto
+                    {
+                        Id = searchResult.Id.VideoId,
+                        Title = searchResult.Snippet.Title,
+                        Description = searchResult.Snippet.Description,
+                        Thumbnail = searchResult.Snippet.Thumbnails.High.Url,
+                        ChannelId = searchResult.Snippet.ChannelId,
+                        ChannelTitle = searchResult.Snippet.ChannelTitle,
+                        PublishedAt = searchResult.Snippet.PublishedAt ?? DateTime.MinValue
+                    });
+                }
+            }
+
+            return videos;
+        }
+    }
+}
