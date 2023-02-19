0d87ba4dc122bfc1acaadf0176f8b9081b71ae62
EnergyCube <vincent.mardirossian@epitech.eu>
2023-02-19 11:17:45 +0100

    feat(yt/regist): added Youtube related Dto

---
+++ api/youtube/YouPlug/Dto/Youtube/ChannelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace YouPlug.Dto.Youtube
+{
+    public class ChannelDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
--- api/youtube/YouPlug/TubeFetcher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YouPlug
-{
-
-    public class TubeFetcher
-    {
-
-    }
-}
