fecc5b902e1f464e5daaa5ee23004fcad6cbcb49
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-02-20 19:03:00 +0100

    feat(api): should handle right the variables for all discord actions

---
+++ api/discord/src/amqp/amqp.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+
+@Injectable()
+export class AmqpService {
+  private logger = new Logger(AmqpService.name);
+  constructor(private amqpConnection: AmqpConnection) {}
+
+  async publishAction(
+    actionId: string,
+    plugId: string,
+    runId: string,
+    userId: number,
+    variables: { key: string; value: string }[],
+  ) {
+    const queue = 'plug_action_finished';
+    const msg = {
+      serviceName: 'discord',
+      plugId,
+      runId,
+      actionId,
+      userId,
+      variables,
+    };
+
+    this.logger.log(
+      `Publishing to ${queue} with message ${JSON.stringify(msg)}`,
+    );
+    await this.amqpConnection.publish('amq.direct', queue, msg);
+    this.logger.log(`Published to ${queue}`);
+  }
+
+  async publishEvent(
+    queue: string,
+    stepId: string,
+    userId: number,
+    variables: { key: string; value: string }[],
+  ) {
+    const msg = {
+      serviceName: 'discord',
+      eventId: stepId,
+      userId,
+      variables,
+    };
+
+    this.logger.log(
+      `Publishing to ${queue} with message ${JSON.stringify(msg)}`,
+    );
+    await this.amqpConnection.publish('amq.direct', queue, msg);
+    this.logger.log(`Published to ${queue}`);
+  }
+}
