740e06df7c9303ac65be45e39b83942cffa9a30e
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-02-20 15:09:21 +0100

    feat(api): check if the channel is inside the connected server

--- api/discord/src/app.controller.ts
+++ api/discord/src/app.controller.ts
@@ -1,11 +1,15 @@
 import { RabbitSubscribe, Nack } from '@golevelup/nestjs-rabbitmq';
 import { Controller, Logger } from '@nestjs/common';
 import { DiscordService } from './discord/discord.service';
+import { DiscordAuthService } from './discord/discordAuth.service';
 
 @Controller()
 export class AppController {
   private logger = new Logger(AppController.name);
-  constructor(private discordService: DiscordService) {}
+  constructor(
+    private discordService: DiscordService,
+    private discordAuthService: DiscordAuthService,
+  ) {}
 
   @RabbitSubscribe({
     queue: 'plug_action_discord_triggers',
@@ -13,8 +17,8 @@ export class AppController {
   async triggerAction(msg: any) {
     this.logger.log(`Received event trigger: ${JSON.stringify(msg)}`);
     const { actionId, userId } = msg;
-    let content;
-    let id;
+    let content: string;
+    let id: string;
 
     switch (actionId) {
       case 'pm':
@@ -29,8 +33,11 @@ export class AppController {
           (field: any) => field.key === 'content',
         ).value;
         id = msg.fields.find((field: any) => field.key === 'id').value;
+        const { serverId } = await this.discordAuthService.retrieveByUserId(
+          userId,
+        );
 
-        await this.discordService.sendChannelMessage(id, content);
+        await this.discordService.sendChannelMessage(serverId, id, content);
     }
     return new Nack(false);
   }
