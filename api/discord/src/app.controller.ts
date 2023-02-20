f7e624d781c5e53624c0305ecbd5a30918e013c4
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-02-20 18:25:49 +0100

    feat(api): handle all the actions inside discord

--- api/discord/src/app.controller.ts
+++ api/discord/src/app.controller.ts
@@ -17,27 +17,188 @@ export class AppController {
   async triggerAction(msg: any) {
     this.logger.log(`Received event trigger: ${JSON.stringify(msg)}`);
     const { actionId, userId } = msg;
-    let content: string;
-    let id: string;
+    const { serverId } = await this.discordAuthService.retrieveByUserId(userId);
 
     switch (actionId) {
       case 'pm':
-        content = msg.fields.find(
+        const messageContent = msg.fields.find(
           (field: any) => field.key === 'content',
         ).value;
-        id = msg.fields.find((field: any) => field.key === 'id').value;
+        const userId2 = msg.fields.find(
+          (field: any) => field.key === 'id',
+        ).value;
 
-        await this.discordService.sendPrivateMessage(id, content);
+        await this.discordService.sendPrivateMessage(userId2, messageContent);
       case 'channel_message':
-        content = msg.fields.find(
+        const messageContent2 = msg.fields.find(
           (field: any) => field.key === 'content',
         ).value;
-        id = msg.fields.find((field: any) => field.key === 'id').value;
-        const { serverId } = await this.discordAuthService.retrieveByUserId(
-          userId,
+        const channelId = msg.fields.find(
+          (field: any) => field.key === 'id',
+        ).value;
+
+        await this.discordService.sendChannelMessage(
+          serverId,
+          channelId,
+          messageContent2,
+        );
+      case 'public_thread_create':
+        const messageId = msg.fields.find(
+          (field: any) => field.key === 'message_id',
+        ).value;
+        const name = msg.fields.find(
+          (field: any) => field.key === 'name',
+        ).value;
+        const reason = msg.fields.find(
+          (field: any) => field.key === 'reason',
+        )?.value;
+        const auto_archive_duration = msg.fields.find(
+          (field: any) => field.key === 'auto_archive_duration',
+        )?.value;
+        const rate_limit_per_user = msg.fields.find(
+          (field: any) => field.key === 'rate_limit_per_user',
+        )?.value;
+
+        await this.discordService.createPublicThread(
+          serverId,
+          messageId,
+          name,
+          reason,
+          auto_archive_duration,
+          rate_limit_per_user,
         );
 
-        await this.discordService.sendChannelMessage(serverId, id, content);
+      // TOOO: publish the thread id
+      case 'private_thread_create':
+        const channelId2 = msg.fields.find(
+          (field: any) => field.key === 'channel_id',
+        ).value;
+        const name2 = msg.fields.find(
+          (field: any) => field.key === 'name',
+        ).value;
+        const auto_archive_duration2 = msg.fields.find(
+          (field: any) => field.key === 'auto_archive_duration',
+        )?.value;
+        const rate_limit_per_user2 = msg.fields.find(
+          (field: any) => field.key === 'rate_limit_per_user',
+        )?.value;
+
+        await this.discordService.createPrivateThread(
+          serverId,
+          channelId2,
+          name2,
+          auto_archive_duration2,
+          rate_limit_per_user2,
+        );
+
+      // TODO: publish the thread id
+      case 'thread_delete':
+        const threadId = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+
+        await this.discordService.deleteThread(serverId, threadId);
+      case 'send_message_thread':
+        const threadId2 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+        const messageContent3 = msg.fields.find(
+          (field: any) => field.key === 'content',
+        ).value;
+
+        await this.discordService.sendMessageInThread(
+          serverId,
+          threadId2,
+          messageContent3,
+        );
+      case 'archive_thread':
+        const threadId3 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+
+        await this.discordService.archiveThread(serverId, threadId3, true);
+      case 'unarchive_thread':
+        const threadId4 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+
+        await this.discordService.archiveThread(serverId, threadId4, false);
+      case 'lock_thread':
+        const threadId5 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+
+        await this.discordService.lockThread(serverId, threadId5, true);
+      case 'unlock_thread':
+        const threadId6 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+
+        await this.discordService.lockThread(serverId, threadId6, false);
+      case 'reply_message':
+        const messageId2 = msg.fields.find(
+          (field: any) => field.key === 'message_id',
+        ).value;
+        const messageContent4 = msg.fields.find(
+          (field: any) => field.key === 'message',
+        ).value;
+
+        await this.discordService.replyToChannelMessage(
+          serverId,
+          messageId2,
+          messageContent4,
+        );
+      case 'react_to_message':
+        const messageId3 = msg.fields.find(
+          (field: any) => field.key === 'message_id',
+        ).value;
+        const reaction = msg.fields.find(
+          (field: any) => field.key === 'reaction',
+        ).value;
+
+        await this.discordService.sendReactionToChannelMessage(
+          serverId,
+          messageId3,
+          reaction,
+        );
+      case 'publish_message':
+        const messageId4 = msg.fields.find(
+          (field: any) => field.key === 'message_id',
+        ).value;
+
+        await this.discordService.publishMessage(serverId, messageId4);
+      case 'add_member_to_thread':
+        const threadId7 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+        const userId3 = msg.fields.find(
+          (field: any) => field.key === 'user_id',
+        ).value;
+
+        await this.discordService.addMemberToThread(
+          serverId,
+          threadId7,
+          userId3,
+        );
+      case 'remove_member_from_thread':
+        const threadId8 = msg.fields.find(
+          (field: any) => field.key === 'thread_id',
+        ).value;
+        const userId4 = msg.fields.find(
+          (field: any) => field.key === 'user_id',
+        ).value;
+
+        await this.discordService.removeMemberFromThread(
+          serverId,
+          threadId8,
+          userId4,
+        );
+      case 'delete_message':
+        const messageId5 = msg.fields.find(
+          (field: any) => field.key === 'message_id',
+        ).value;
+
+        await this.discordService.deleteMessage(serverId, messageId5);
     }
     return new Nack(false);
   }
