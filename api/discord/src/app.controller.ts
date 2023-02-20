0e272945a520379dfc896b1f3ec28cde42545561
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-02-20 23:06:58 +0100

    fix(api): switch case for discord should works

--- api/discord/src/app.controller.ts
+++ api/discord/src/app.controller.ts
@@ -39,6 +39,7 @@ export class AppController {
             value: message.id,
           },
         ];
+        break;
       case 'channel_message':
         const messageContent2 = msg.fields.find(
           (field: any) => field.key === 'content',
@@ -58,6 +59,7 @@ export class AppController {
             value: message2.id,
           },
         ];
+        break;
       case 'public_thread_create':
         const messageId = msg.fields.find(
           (field: any) => field.key === 'message_id',
@@ -90,6 +92,7 @@ export class AppController {
             value: thread.id,
           },
         ];
+        break;
       case 'private_thread_create':
         const channelId2 = msg.fields.find(
           (field: any) => field.key === 'channel_id',
@@ -118,6 +121,7 @@ export class AppController {
             value: thread2.id,
           },
         ];
+        break;
 
       case 'thread_delete':
         const threadId = msg.fields.find(
@@ -144,30 +148,35 @@ export class AppController {
             value: message3.id,
           },
         ];
+        break;
       case 'archive_thread':
         const threadId3 = msg.fields.find(
           (field: any) => field.key === 'thread_id',
         ).value;
 
         await this.discordService.archiveThread(serverId, threadId3, true);
+        break;
       case 'unarchive_thread':
         const threadId4 = msg.fields.find(
           (field: any) => field.key === 'thread_id',
         ).value;
 
         await this.discordService.archiveThread(serverId, threadId4, false);
+        break;
       case 'lock_thread':
         const threadId5 = msg.fields.find(
           (field: any) => field.key === 'thread_id',
         ).value;
 
         await this.discordService.lockThread(serverId, threadId5, true);
+        break;
       case 'unlock_thread':
         const threadId6 = msg.fields.find(
           (field: any) => field.key === 'thread_id',
         ).value;
 
         await this.discordService.lockThread(serverId, threadId6, false);
+        break;
       case 'reply_message':
         const messageId2 = msg.fields.find(
           (field: any) => field.key === 'message_id',
@@ -187,6 +196,7 @@ export class AppController {
             value: message4.id,
           },
         ];
+        break;
       case 'react_to_message':
         const messageId3 = msg.fields.find(
           (field: any) => field.key === 'message_id',
@@ -200,12 +210,14 @@ export class AppController {
           messageId3,
           reaction,
         );
+        break;
       case 'publish_message':
         const messageId4 = msg.fields.find(
           (field: any) => field.key === 'message_id',
         ).value;
 
         await this.discordService.publishMessage(serverId, messageId4);
+        break;
       case 'add_member_to_thread':
         const threadId7 = msg.fields.find(
           (field: any) => field.key === 'thread_id',
@@ -219,6 +231,7 @@ export class AppController {
           threadId7,
           userId3,
         );
+        break;
       case 'remove_member_from_thread':
         const threadId8 = msg.fields.find(
           (field: any) => field.key === 'thread_id',
@@ -232,12 +245,14 @@ export class AppController {
           threadId8,
           userId4,
         );
+        break;
       case 'delete_message':
         const messageId5 = msg.fields.find(
           (field: any) => field.key === 'message_id',
         ).value;
 
         await this.discordService.deleteMessage(serverId, messageId5);
+        break;
     }
   } catch (err) {
     this.logger.log("Can't execute an action in discord", err);
