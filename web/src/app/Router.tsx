b2a57bc719681028d3ef70ad85a82735d0d9d84f
0xMemoryGrinder <alexjublot@gmail.com>
2023-02-21 23:51:38 +0100

    feat(web): added NavigablePage component for all pages with navbar

--- web/src/app/Router.tsx
+++ web/src/app/Router.tsx
@@ -7,17 +7,18 @@ import SignupPage from '../pages/SignupPage';
 import HomePage from '../pages/HomePage';
 import PlugCreatePage from '../pages/PlugCreatePage';
 import PlugEditPage from '../pages/PlugEditPage';
+import { NavigablePage } from './NavigablePage';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/services" element={<ServicesPage />} />
-      <Route path="/plugs" element={<PlugsPage />} />
-      <Route path="/plugs/create" element={<PlugCreatePage />} />
-      <Route path="/plugs/edit/:plugId" element={<PlugEditPage />} />
+      <Route path="/" element={<NavigablePage page={<HomePage />} />} />
+      <Route path="/login" element={<NavigablePage page={<LoginPage />} />} />
+      <Route path="/signup" element={<NavigablePage page={<SignupPage />} />} />
+      <Route path="/services" element={<NavigablePage page={<ServicesPage />} />} />
+      <Route path="/plugs" element={<NavigablePage page={<PlugsPage />} />} />
+      <Route path="/plugs/create" element={<NavigablePage page={<PlugCreatePage />} />} />
+      <Route path="/plugs/edit/:plugId" element={<NavigablePage page={<PlugEditPage />} />} />
       <Route path="*" element={<Navigate to={'/'} replace />} />
     </Routes>
   </BrowserRouter>
