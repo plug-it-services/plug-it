63f721bcd77900d7297f16e612d4ed52f494d896
0xMemoryGrinder <alexjublot@gmail.com>
2023-02-21 23:51:08 +0100

    feat(web): added MDBootstrap package

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
