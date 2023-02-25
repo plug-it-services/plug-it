6caa7367496bffc17bc9ee5cb8d1b5d6aadc31f5
EnergyCube <vincent.mardirossian@epitech.eu>
2023-02-25 20:42:19 +0100

    feat(yt/regist): added nullable Thumbnail

--- api/youtube/YouPlug/Dto/Youtube/VideoDto.cs
+++ api/youtube/YouPlug/Dto/Youtube/VideoDto.cs
@@ -5,7 +5,7 @@
         public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Thumbnail { get; set; }
+        public string? Thumbnail { get; set; }
         public string ChannelId { get; set; }
         public string ChannelTitle { get; set; }
         public DateTime PublishedAt { get; set; }
