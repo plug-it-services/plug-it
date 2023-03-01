1fd7c3e43517cb2c96740a188b01b5b19f4dadf2
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-03-01 16:51:01 +0100

    feat(api): init request for etherscan

--- api/etherscan/main.go
+++ api/etherscan/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/plug-it-services/plug-it/models"
 	"github.com/plug-it-services/plug-it/routers"
@@ -18,10 +24,23 @@ func startServer() {
 	}
 }
 
+func initRequest() {
+	content, err := ioutil.ReadFile("./config/etherscan.json")
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	_, err = http.Post(viper.Get("PLUGS_SERVICE_INITIALIZE_URL").(string), "application/json", bytes.NewBuffer(content))
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
 func main() {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
 
 	models.SetupModels()
+	initRequest()
 	startServer()
 }
