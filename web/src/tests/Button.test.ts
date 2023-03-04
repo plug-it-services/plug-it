ec9b87d0af0e16f3a277cedb9ba456a6bd681a0c
0xSwapFeeder <72012341+0xSwapFeeder@users.noreply.github.com>
2023-03-04 22:09:44 +0000

    feat(web/tests): added base tests features

---
+++ web/src/tests/Button.test.ts
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Button from "../components/Button";
+import '@testing-library/jest-dom/extend-expect';
+
+describe("Plug Button", () => {
+  it("renders with the correct text", () => {
+    render(Button({ text:"Click me" }));
+    expect(screen.getByText("Click me")).toBeInTheDocument();
+  });
+});
