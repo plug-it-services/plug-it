ed3f0951f5e9fd158d556a23b67546c77ec7117a
0xSwapFeeder <72012341+0xSwapFeeder@users.noreply.github.com>
2023-03-04 22:21:53 +0000

    feat(web/tests): added unit tests

--- web/src/tests/Button.test.ts
+++ web/src/tests/Button.test.ts
@@ -1,11 +1,10 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Button from "../components/Button";
+import { render, screen } from '@testing-library/react';
+import Button from '../components/Button';
 import '@testing-library/jest-dom/extend-expect';
 
-describe("Plug Button", () => {
-  it("renders with the correct text", () => {
-    render(Button({ text:"Click me" }));
-    expect(screen.getByText("Click me")).toBeInTheDocument();
+describe('Plug Button', () => {
+  it('renders with the correct text', () => {
+    render(Button({ text: 'Click me' }));
+    expect(screen.getByText('Click me')).toBeInTheDocument();
   });
 });
---
+++ web/src/tests/InputBar.test.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import InputBar from '../components/InputBar';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Plug InputBar', () => {
+  it('renders with the correct text', () => {
+    render(
+      InputBar({
+        placeholder: 'Click me',
+        textColor: '#000000',
+        backgroundColor: '#000000',
+        borderColor: '#000000',
+        isPassword: false,
+        onChange: () => {},
+        onSubmit: () => {},
+        value: 'Click me',
+      }),
+    );
+    expect(screen.getByDisplayValue('Click me')).toBeInTheDocument();
+  });
+});
