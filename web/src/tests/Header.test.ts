4a1b2c9f4b20dabb57f902c37f836020e6dd3905
EnergyCube <vincent.mardirossian@epitech.eu>
2023-03-04 23:58:33 +0100

    feat(api/tests): added more testing for header

--- web/src/tests/Header.test.ts
+++ web/src/tests/Header.test.ts
@@ -18,3 +18,71 @@ describe('Plug Button', () => {
     expect(screen.getByText('Wow')).toBeInTheDocument();
   });
 });
+
+describe('Plug AdvancedButton', () => {
+  it('renders with the correct text', () => {
+    render(
+      MessageBox({
+        description: 'Advanced',
+        title: 'Title Advanced',
+        isOpen: true,
+        onClose: () => {},
+        children: null,
+        type: 'success',
+      }),
+    );
+    expect(screen.getByText('Title Advanced')).toBeInTheDocument();
+    expect(screen.getByText('Advanced')).toBeInTheDocument();
+  });
+});
+
+describe('Plug Button Error', () => {
+  it('renders with the correct text', () => {
+    render(
+      MessageBox({
+        description: 'Error',
+        title: 'Title',
+        isOpen: true,
+        onClose: () => {},
+        children: null,
+        type: 'error',
+      }),
+    );
+    expect(screen.getByText('Title')).toBeInTheDocument();
+    expect(screen.getByText('Error')).toBeInTheDocument();
+  });
+});
+
+describe('Plug Button Info', () => {
+  it('renders with the correct text', () => {
+    render(
+      MessageBox({
+        description: 'Wow',
+        title: 'Title',
+        isOpen: true,
+        onClose: () => {},
+        children: null,
+        type: 'info',
+      }),
+    );
+    expect(screen.getByText('Title')).toBeInTheDocument();
+    expect(screen.getByText('Wow')).toBeInTheDocument();
+  });
+});
+
+describe('Plug Button Warning', () => {
+  it('renders with the correct text', () => {
+    render(
+      MessageBox({
+        description: 'Wow',
+        title: 'Title',
+        isOpen: true,
+        onClose: () => {},
+        children: null,
+        type: 'warning',
+      }),
+    );
+    expect(screen.getByText('Title')).toBeInTheDocument();
+    expect(screen.getByText('Wow')).toBeInTheDocument();
+  });
+});
